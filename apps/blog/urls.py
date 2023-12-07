5e5a9e0452e122975b2780f493a7cffc53274e32
francisco cabaña <franciscocabana82@gmail.com>
2023-12-07 19:35:15 -0300

    Crud cancion

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path 
-from .views import (DiscoListView, DiscoDetailView, DiscoCreateView, DiscoUpdateview, DiscoDeleteView)
+from .views import (
+    DiscoListView, DiscoDetailView, DiscoCreateView, DiscoUpdateview, DiscoDeleteView,
+    CancionListView, CancionDetailView, CancionCreateView, CancionUpdateview, CancionDeleteView)
 urlpatterns = [
     path('disco/list/', DiscoListView.as_view(), name= 'disco_list'),
     path('disco/<int:pk>/', DiscoDetailView.as_view(), name='disco_detail'),
     path('disco/create/', DiscoCreateView.as_view(), name='disco_create'),
     path('disco/<int:pk>/update/', DiscoUpdateview.as_view(), name='disco_update'),
     path('disco/<int:pk>/delete/', DiscoDeleteView.as_view(), name='disco_delete'),
+    
+    path('cancion/list/', CancionListView.as_view(), name= 'cancion_list'),
+    path('cancion/<int:pk>/', CancionDetailView.as_view(), name='cancion_detail'),
+    path('cancion/create/', CancionCreateView.as_view(), name='cancion_create'),
+    path('cancion/<int:pk>/update/', CancionUpdateview.as_view(), name='cancion_update'),
+    path('cancion/<int:pk>/delete/', CancionDeleteView.as_view(), name='cancion_delete'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Disco
+from .models import Disco, Cancion
 
 class DiscoListView(ListView):
     model = Disco
@@ -31,6 +31,36 @@ class DiscoDeleteView(DeleteView):
     template_name = 'blogs/discos/disco_confirm_delete.html'
     context_object_name = 'discos'
     success_url = reverse_lazy('disco_list')
+
+class CancionListView(ListView):
+    model = Cancion
+    template_name = 'blogs/canciones/cancion_list.html'
+    context_object_name = 'canciones'
+
+class CancionDetailView(DetailView):
+    model = Cancion
+    template_name = 'blogs/canciones/cancion_detail.html'
+    context_object_name = 'cancion'
+
+class CancionCreateView(CreateView):
+    model = Cancion
+    template_name = 'blogs/canciones/cancion_form.html'
+    context_object_name = 'cancion'
+    fields = '__all__'
+    success_url = reverse_lazy('cancion_list')
+
+class CancionUpdateview(UpdateView):
+    model = Cancion
+    template_name = 'blogs/canciones/cancion_form.html'
+    context_object_name = 'cancion'
+    fields = '__all__'
+    success_url = reverse_lazy('cancion_list')
+
+class CancionDeleteView(DeleteView):
+    model = Cancion
+    template_name = 'blogs/canciones/cancion_confirm_delete.html'
+    context_object_name = 'cancion'
+    success_url = reverse_lazy('cancion_list')
     
 
 # Create your views here.
